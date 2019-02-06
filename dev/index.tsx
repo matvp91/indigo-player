d11088b9cf3b0cc8e2df92a4c9ad3e4c2c4df841
Matthias <matvp91@gmail.com>
2019-02-06 17:14:29 +0100

    Removed unused code.

--- dev/index.tsx
+++ dev/index.tsx
@@ -42,9 +42,6 @@ const player = IndigoPlayer.init(document.getElementById('playerContainer'), {
   //   // 'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=',
   //   src: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=',
   // },
-  vimeo: {
-    id: '112667959',
-  },
   sources: [
     // {
     //   type: 'hls',
